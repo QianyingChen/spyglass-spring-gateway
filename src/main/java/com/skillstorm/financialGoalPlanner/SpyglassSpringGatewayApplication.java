31927bde6bcd7be3def4da58d2726ff078f64882
Teresa Chen <qianyingteresachen@gmail.com>
2023-06-21 17:25:01 -0400

    add stater project

---
+++ src/main/java/com/skillstorm/financialGoalPlanner/SpyglassSpringGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.financialGoalPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyglassSpringGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyglassSpringGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillstorm/financialGoalPlanner/SpyglassSpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.financialGoalPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyglassSpringGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
